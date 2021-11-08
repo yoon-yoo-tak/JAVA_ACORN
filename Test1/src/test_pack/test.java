0360c23277927a764c9f7751c5fa3ad6bcd22959
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2021-11-08 09:55:31 +0900

    test

---
+++ Test1/src/test_pack/test.java
@@ -0,0 +1,17 @@
+package test_pack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		
+		System.out.println(a);
+	}
+
+}
